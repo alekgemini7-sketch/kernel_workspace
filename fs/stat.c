c30e017f692d9e0e78a3b0ef803e467ab28cee6e
alekgemini7-sketch <alekgemini7@gmail.com>
2026-09-21 19:37:22 +0900

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -171,12 +171,13 @@ extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *fla
 int vfs_statx(int dfd, const char __user *filename, int flags,
 	      struct kstat *stat, u32 request_mask)
 {
-	#ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flags);
-#endif
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
+
+	#ifdef CONFIG_KSU
+	ksu_handle_stat(&dfd, &filename, &flags);
+	#endif
 
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
